33c76bd3e484838f87c8ed4e50f26528c75a6ab4
Nathan Otto Siemers <nathan.siemers@bms.com>
2019-09-21 10:11:24 -0700

    documentation. removed sqldf library, not needed.

--- Data/defaults.template.R
+++ Data/defaults.template.R
@@ -15,11 +15,6 @@ library(rlang)
 sc_env = new.env()
 ## you must write a loader, no matter how trivial
 sc_env$db_rds_name = 'schcc.rds' ## name of final rds file written
-source('Data/load.template.R')
-## we load data into R memory, as we may be asked to compute
-## correlations from slices of it on-the-fly
-sc_env$data = load_data( sc_env$db_rds_name )
-
 ## project annotation that shows up in ui
 
 sc_env$PI = "Nathan Siemers"
@@ -45,4 +40,66 @@ sc_env$default.genelist = c('CD3E', 'CD8A', 'CD4', 'FOXP3')
 sc_env$default.colorby= ""
 sc_env$default.filterby = "Tissue"
 env_print(sc_env)
+
+## your loader should create/return a data frame with genes and sample annotation in columns
+
+load_data =  function( db_rds_name, create = FALSE, testing = TRUE ) {
+    if (create) {
+        ## create load data from scratch
+        ## how you do this will depend on your files and formats  available
+        ## if you harmonize to gene symbol and other identifers supplied
+        ## this will involve more effort
+        ## you may need to decide how to treat multiple samples with same symbol
+        ## below is an example (Bejing University Single Cell Hepatocellular Carcinoma)
+        ##library(sqldf)
+        library(data.table)
+        library(tidyverse)
+        ## EXPRESSION MATRIX
+        dat = read.csv('Data/default.GSE98638_HCC.TCell.S5063.count.txt.gz', sep = '\t', stringsAsFactors = FALSE )
+        dat = dat %>% filter( !is.na(symbol) ) %>% dplyr::select(-geneID) %>%
+            as.data.table
+        ################################################################
+        ## jump into data.table land to transpose, etc
+        ## I'm no wiz with data.tables, but recommend for large operations
+        dat = dat[, lapply(.SD, sum, na.rm=TRUE), by='symbol', .SDcols=sapply(dat, is.numeric) ]
+        ## save symbols and samples for use later
+        symbols = dat$symbol; samples = colnames(dat)[-1]
+        dat = data.table::transpose(dat[,-1])
+        ## transformation? normalization? filtering? do it here.
+        dat = log2(dat + 1)
+        ## get out of data.table land
+        dat = as.data.frame(dat)
+        ################################################################
+        ## add row, column names
+        colnames(dat) = symbols; dat$sample = samples
+        ## you keep a copy expression-only data if you wish
+        if(FALSE) saveRDS(dat, file='Data/data.rds')
+        ## SAMPLE ANNOTATIONS (phenotypes/pheno)
+        ## clean up as you wish
+        pheno = read.csv('Data/default.pheno.data', header = FALSE, sep = '\t', skip = 1)
+        colnames(pheno) = c('sample', 'Patient', 'Cluster', 'Tissue')
+        pheno$sample = gsub('-', '.', pheno$sample, fixed = TRUE)
+        pheno$Tissue = gsub('PT.*', 'Periph', pheno$Tissue)
+        pheno$Tissue = gsub('JT.*', 'J Unk', pheno$Tissue)
+        pheno$Tissue = gsub('TT.*', 'Tumor', pheno$Tissue)
+        pheno$Tissue = gsub('NT.*', 'Adj Norm', pheno$Tissue)
+        ## save a copy if desired
+        if(FALSE) saveRDS ( pheno, file = 'Data/pheno.rds' )
+        ## MERGE
+        dat = pheno %>%
+            filter(Tissue != 'J Unk') %>%
+                left_join( dat, by = "sample" )
+        saveRDS( dat, file.path('Data', db_rds_name ) )
+    }
+    dat = readRDS( file.path('Data', db_rds_name ) )
+    dat
+}
+
+## we load data into R memory, as we may be asked to compute
+## correlations from slices of it on-the-fly
+sc_env$data = load_data( sc_env$db_rds_name )
+
+
+
+
 
--- README.md
+++ README.md
@@ -1,21 +1,59 @@
-# Single Cell 'Faux Flow' code base
+# FauxFlow: a graphics library and shiny tool for visualizing single cell data in a flow cytometry-like paradigm.
 
 Nathan Siemers
 
-## Instructions:
+## How to use Fauxflow
+
+### library dependencies:
+
+*tidyverse, ggthemes, GGally, lazyeval, shiny, shinyjs, shinythemes, shinycssloaders, viridis, MASS, dplyr, rlang*
+
+The *data.table* package is also used during the process of data set construction.
+
+
+### out of the box: Zheng HCC data
+
+The tool comes with the Zheng et al. single cell hepatocellular carcinoma T cell dataset available.  http://dx.doi.org/10.1016/j.cell.2017.05.035
+
+If you have a shiny server available, the tool should be ready for use with the Zheng data. If you don't have a shiny server, you can run an instance of the tool as follows:
+
+*R -e 'shiny::runApp("./", port=8888, host="0.0.0.0")'*
+
+### adding your own data sets
 
 copy Data/defaults.R.template to Data/defaults.R
 
-customize Data/defaults.R
+edit Data/defaults.R:
 
-load_data() should return a single data frame of combined rna-seq and clinical information (genes and clinical categories in columns)
+* Fill in appropriate background information about the data set.
+* Make a list of column names that are clinical/sample annotations (sc_env$clin.cols), these columns will be treated differently in some cases (gaussian noise is added to numeric data, but not on the columns specified here).
+* write a loader (see the load_data() function)
 
-load_data should also push the data frame into the global environment, so that any future load within a shiny session happen instantly.  See the example.
+The loader should return a single data frame of combined rna-seq and clinical information (genes and clinical categories in columns).  There's almost always some tweaking to do.
 
-Modify the other variables (site title, default choices, etc).
+* Finally: set TESTING to FALSE in global.R, this will source your new Data/defaults.R instead of defaults.template.R
 
-## This template was written such that you should be able to apply it to an existing directory without damaging the site. You can do this via:
 
-git clone git@biogit.pri.bms.com:siemersn/Single-Cell-Template.git tmp && rsync -av tmp/ .  && rm -rf  tmp
+
+## lib/scplotlib/scplotlib.R
+
+The graphics engine for FauxFlow is contained here in the *allpairs()* and underlying *gggpairs()* functions.  The engine uses GGally::ggpairs extensively, but with heavy customization of underlying plotting functions that get called for each subplot.
+
+## Code updates
+
+You should be able to pull updates from the git repository without harming things, as long as you have made a custom Data/defaults.R file. The only thing that needs changing after a pull is setting TESTING = FALSE in globals.R, so that your custom data is used.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
