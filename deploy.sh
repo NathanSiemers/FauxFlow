649b196967786cf7ed8155b69602a4cc15da9468
Nathan Otto Siemers <nathan.siemers@bms.com>
2019-10-29 14:31:14 -0700

    added deploy script; last commit before megaflow

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+git commit -a
+git push origin master
+git push github master
+rsync -av *.R template.docx *.Rmd restart.txt lib ../SCHCC/
+rsync -av *.R template.docx *.Rmd restart.txt lib ../SCBlood/
+rsync -av *.R template.docx *.Rmd restart.txt lib ../SCMel/
+rsync -av *.R template.docx *.Rmd restart.txt lib ec2-user@shiny.rwc.bms.com:Shiny/SCHCC/
+rsync -av *.R template.docx *.Rmd restart.txt lib ec2-user@shiny.rwc.bms.com:Shiny/SCBlood/
+rsync -av *.R template.docx *.Rmd restart.txt lib ec2-user@shiny.rwc.bms.com:Shiny/SCMel/
+rsync -av *.R template.docx *.Rmd restart.txt lib nathan@shiny.fiveprime.org:Shiny/SCHCC/
+rsync -av *.R template.docx *.Rmd restart.txt lib nathan@shiny.fiveprime.org:Shiny/SCBlood/
+rsync -av *.R template.docx *.Rmd restart.txt lib nathan@shiny.fiveprime.org:Shiny/SCMel/
+
+
